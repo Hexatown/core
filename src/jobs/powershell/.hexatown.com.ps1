ed95e87934dbcd203102ee0e2b47bfcc24958afb
Niels Gregers Johansen <niels@timeboxer.dk>
2021-03-20 10:35:23 +0100

    v2.0.0 Breaking change to keep authencating to Exchange using username / password supply option when setting up the context connectToExchangeUserNamePassword=$true

--- src/jobs/powershell/.hexatown.com.ps1
+++ src/jobs/powershell/.hexatown.com.ps1
@@ -1,15 +1,21 @@
-<# V1.1.13@HEXATOWN 
+<# V2.0.0@HEXATOWN 
  
-Copyright (C) 2020-2021 Niels Gregers Johansen.
+Copyright (C) 2020-2021 Niels Gregers Johansen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+2.0.0
+------
+Breaking change, to keep authencating to Exchange using
+username / password supply option when setting up the context 
+connectToExchangeUserNamePassword=$true
 1.1.13
 -----
 - Extended  Send-Hexatown-Mail with a new parameter ContentType defaulting to the original Text value - KUDO lbocak
+
 1.1.12
 ------
 - Included core sharepoint list defintions
@@ -891,8 +897,11 @@ function Init ($invocation, $requireExchange, $delegated,$scope,$otherOptions) {
         Import-Module -Name $modulePath -DisableNameChecking
     
             $errorCount = (RealErrorCount)
-            #$session = ConnectExchange $env:AADUSER $env:AADPASSWORD
+            if ($true -eq $otherOptions.connectToExchangeUserNamePassword){
+                $session = ConnectExchange $env:AADUSER $env:AADPASSWORD
+            }else{
             $session = ConnectExchange2 $env:APPCLIENT_ID $token
+            }     
             if ($errorCount -ne (RealErrorCount)) {
                 Write-Warning "Cannot connect to Exchange"
                 Done $hexatown
@@ -1452,10 +1461,23 @@ try
                 $ResponseCode = 401
             }else
             {
+
+                if (!(test-path "$PSScriptRoot/../../apis$path/$method.sample.json")) {
+                    $requestProperties = $payload | Get-Member -MemberType  NoteProperty | Select Name, Definition
+                    ConvertTo-Json -InputObject $requestProperties  -Depth 10 | Out-File "$PSScriptRoot/../../apis$path/$method.sample.json" 
+
+                }
+                if (!(test-path "$PSScriptRoot/../../apis$path/$method.fields.json")) {
+
+                    $requestFields = $payload | Get-Member -MemberType  NoteProperty | Select Name
+                    ConvertTo-Json -InputObject $requestFields  -Depth 10 | Out-File "$PSScriptRoot/../../apis$path/$method.fields.json" 
+                }
+
                 $responseData = & "$PSScriptRoot/../../apis/$path/$method.ps1" $hexatown $payload $requestor
                 
                 
                 $ResponseCode = 200
+                
             }        
         
         }
@@ -1477,7 +1499,7 @@ try
 
      
 $body = @{fields = @{
-     #                 Processed = $true
+                      Processed = $true
                       Responsecode = $ResponseCode
                       Response = $responseJSON
                       ResponseCSV = $responseCSV 
